566b2bdec8880bd6bc097e94c7fe68b165f146e1
JoeyNew2025 <ychaoae@connect.ust.hk>
2025-09-22 15:24:27 +0800

    Update main.py
    change made to develop the main branch after a newbrach has been pushed from by local computer.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 print("I created an account on GitHub")
 Print("I created a repository on GitHub")
 Print("I will test whether I can clone this repository to my local linux on windowns.")
+
+print("I wrote this as a change to develop the main branch, after the GitHub has received the newbranch pushed back from my local computer.")
+print("I will then pull this out to my local computer")
