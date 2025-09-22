55d3712c8ead9552269d7fef5dfeffc89ba3206a
Joey <ychaoae@connect.ust.hk>
2025-09-22 15:14:50 +0800

    I started a newbranch on my own computer

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 print("I created an account on GitHub")
 Print("I created a repository on GitHub")
 Print("I will test whether I can clone this repository to my local linux on windowns.")
+
+
+
+print("This is the change made on my local comput, and is accounted as the newbranch.")
+
+
+
